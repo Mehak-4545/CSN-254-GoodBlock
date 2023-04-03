01bed15444c05a5ac3f91b87a1b70398c36bf55b
Manan Garg <manantechy@gmail.com>
2023-04-03 18:46:01 +0530

    initial frontend commit

---
+++ my_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class GoodBlock extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
+
+class TestHome extends StatefulWidget {
+  const TestHome({super.key});
+
+  @override
+  State<TestHome> createState() => _TestHomeState();
+}
+
+class _TestHomeState extends State<TestHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
