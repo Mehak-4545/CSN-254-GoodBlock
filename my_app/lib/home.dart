8aace6355cc7a977431e3d8f797bec3751730839
NehaGujar1 <gujarnehap@gmail.com>
2023-04-12 23:52:01 +0530

    Launch screen Profile screen

---
+++ my_app/lib/home.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'main.dart';
+
+void main() {
+  runApp(const SampleApp());
+}
+
+void tmpFunction1() {
+  print('Funt ngo');
+}
+
+void tmpFunction2() {
+  print('Funt indv');
+}
+
+void tmpFunction3() {
+  print('Funt corp');
+}
+
+class SampleApp extends StatelessWidget {
+  const SampleApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  const SampleAppPage({super.key});
+
+  @override
+  State<SampleAppPage> createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  // Default placeholder text.
+  String textToShow = 'I Like Flutter';
+
+  void _updateText() {
+    setState(() {
+      // Update the text.
+      textToShow = 'Flutter is Awesome!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Color.fromARGB(255, 10, 63, 86) ,
+      backgroundColor: Color.fromARGB(255, 198, 234, 250),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          backgroundColor: Color.fromARGB(255, 169, 212, 230),
+          leading: Image.asset(
+            'assets/images/icon.jpeg',
+            scale: 1,
+          ),
+          leadingWidth: 70,
+          title: Container(
+              // child:Image.asset('assets/images/top_bar.jpeg'),
+              ),
+          actions: [Image.asset('assets/images/options.jpeg')],
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          //child: Card(
+          child: Container(
+            // color: const Color(0xFFF9F9F9),
+            padding: const EdgeInsets.all(25),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 215, // new change
+                  // childAspectRatio:0.8;
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset('assets/images/final_quote2.jpeg'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  
+                  height: 440,
+                  child: GridView.count(
+                    
+                    shrinkWrap: true,
+                    childAspectRatio: 0.8,
+                    scrollDirection: Axis.vertical,
+                    crossAxisCount: 3,
+                    children: [
+                      SizedBox(
+                        height: 150,
+                        width: 50,
+                        // height: 150,
+                        // color:Color.fromARGB(255, 198, 234, 250),
+                        child: Card(
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: GestureDetector(
+                            onTap: tmpFunction1,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset('assets/images/find.jpeg',height: 150,
+                        width: 50,),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                        width: 50,
+                        // color: Color.fromARGB(255, 198, 234, 250),
+                        child: Card(
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: GestureDetector(
+                            onTap: tmpFunction3,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset('assets/images/history.jpeg',height: 150,
+                        width: 50,),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                        width: 50,
+                        // color: Color.fromARGB(255, 198, 234, 250),
+                        child: Card(
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                              
+                              ),
+                          child: GestureDetector(
+                            
+                            onTap: tmpFunction2,
+                            child: ClipRRect(
+                              
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset('assets/images/about_us.jpeg',height: 150,
+                        width: 50,),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // const Text("He'd have you all unravel at the"),
+                      // const Text('Sound of screams but the'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_back),
+            label: String.fromEnvironment('home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: String.fromEnvironment('home'),
+          ),
+        ],
+        iconSize: 40,
+        elevation: 5,
+        fixedColor: Color.fromARGB(255, 13, 85, 126),
+        backgroundColor: Color.fromARGB(255, 119, 177, 202),
+      ),
+    );
+  }
+}
--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,26 +1,310 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart';
+import 'package:convert/convert.dart';
+import './address.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:ui';
 
-void main() {}
+// Replace these with your contract's ABI and address
+const String contractAbi = """[
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "email",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "password",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "contact",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "Address",
+						"type": "string"
+					}
+				],
+				"indexed": false,
+				"internalType": "struct Corporation.corporation",
+				"name": "",
+				"type": "tuple"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "_createCorp_event",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_userName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_password",
+				"type": "string"
+			}
+		],
+		"name": "_login",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "successful_login",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "contact",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "email",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "Address",
+				"type": "string"
+			}
+		],
+		"name": "_login_event",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_email",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_password",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_contact",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_Address",
+				"type": "string"
+			}
+		],
+		"name": "_signup",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"name": "_signup_event",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Corporations",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "email",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "password",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "contact",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "Address",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]"""; // ABI as a string
+const String contractAddress =
+    '0x07B2Da30288E1BF574F390A0C44ef08cDA4e4617'; // Address as a hex string
 
-class GoodBlock extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ,
-    );
-  }
+late DeployedContract ngoContract, individualContract, corporationContract;
+final Map<String, DeployedContract> allContracts = {
+  "ngo": ngoContract,
+  "individual": individualContract,
+  "corporation": corporationContract
+};
+late Client httpClient;
+late Web3Client client;
+
+void init() async {
+  print(ngo);
+  final ngoAbi = await rootBundle.loadString("../contracts/abi/ngo.json");
+  final individualAbi =
+      await rootBundle.loadString("../contracts/abi/individual.json");
+  final corporationAbi =
+      await rootBundle.loadString("../contracts/abi/corporation.json");
+  ngoContract = DeployedContract(
+    ContractAbi.fromJson(ngoAbi, ngo),
+    EthereumAddress.fromHex(ngo),
+  );
+  corporationContract = DeployedContract(
+    ContractAbi.fromJson(corporationAbi, corporation),
+    EthereumAddress.fromHex(corporation),
+  );
+  individualContract = DeployedContract(
+    ContractAbi.fromJson(individualAbi, individual),
+    EthereumAddress.fromHex(individual),
+  );
+  httpClient = Client();
+  client = Web3Client("https://testnet.aurora.dev/", httpClient);
 }
 
-class TestHome extends StatefulWidget {
-  const TestHome({super.key});
+Future<dynamic> callContractFunction(
+    String contractName, String funcName, List parameter) async {
+  // Encode the function call with its arguments
+  final contract = allContracts[contractName];
+  print("hello");
+  final function = contract!.function(funcName);
+  final transferEvent = contract.event('${funcName}_event');
+  client
+      .events(FilterOptions.events(contract: contract, event: transferEvent))
+      .take(1)
+      .listen((event) {
+    final decoded = transferEvent.decodeResults(event.topics!, event.data!);
 
-  @override
-  State<TestHome> createState() => _TestHomeState();
+    final from = decoded[0];
+    print(
+        '----------------------------------${from[0]}------------------------------------------');
+    return from;
+  });
+  // Get the current gas price
+  final gasPrice = await client.getGasPrice();
+
+  // Create the transaction to call the contract function
+  final transaction = Transaction.callContract(
+    contract: contract,
+    function: function,
+    parameters: parameter,
+    maxGas: 100000,
+    gasPrice: gasPrice,
+    value: EtherAmount.zero(),
+  );
+
+  // Sign and send the transaction
+  final credentials = EthPrivateKey.fromHex(
+      '617bded8c194ba0e803cdb16959c2d6201174ea27f0a2957d523bf141a34b6e3');
+  // final signedTransaction = await client.signTransaction(
+  //   credentials,
+  //   transaction,
+  //   chainId: 1313161555, // Chain ID of the Aurora testnet
+  // );
+  final transactionHash = await client.sendTransaction(credentials, transaction,
+      chainId: 1313161555);
+  print(transactionHash);
+  // Wait for the transaction to be mined
+  dynamic receipt = await client.getTransactionReceipt(transactionHash);
+  print(receipt);
+  while (receipt == null) {
+    receipt = await client.getTransactionReceipt(transactionHash);
+  }
+
+  print("Receipt : ");
+  print(receipt);
+  print("Receipt over");
+
+  // Decode the return value of the contract function
+  dynamic info = await client.getTransactionByHash(transactionHash);
+  print('Return value: ${receipt}');
 }
 
-class _TestHomeState extends State<TestHome> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+void main() {
+  // await callContractFunction();
+  dynamic ans = ngo_signup("Tanmay", "email.com", "vava", "2341", "erwr");
+}
+
+Future<dynamic> ngo_login(String userName, String password) {
+  String functionName = "_login";
+  String contractName = "ngo";
+  List parameter = [userName, password];
+  return callContractFunction(functionName, contractName, parameter);
+}
+
+Future<dynamic> ngo_signup(String userName, String email, String password,
+    String contact, String address) {
+  String functionName = "_signup";
+  String contractName = "ngo";
+  List parameter = [userName, email, password, contact, address];
+  print("ngo signed in");
+  return callContractFunction(functionName, contractName, parameter);
 }
